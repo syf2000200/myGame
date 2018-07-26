be47293681db45e9ad0495b9d1a80f7b2f4881a2
sunyf <sunyf-c@glodon.com>
2018-07-26 09:53:31 +0800

    sss

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -1,22 +1,7 @@
-cc.Class({
-    extends: cc.Component,
-
-    properties: {
-        label: {
-            default: null,
-            type: cc.Label
-        },
-        // defaults, set visually when attaching this script to the Canvas
-        text: 'Hello, World!'
-    },
-
-    // use this for initialization
-    onLoad: function () {
-        this.label.string = this.text;
-    },
-
-    // called every frame
-    update: function (dt) {
-        console,log('updated');
-    },
+const Shape = cc.Class({
+    ctor () {
+        cc.log('shape');
+    }
 });
+
+const shape = new Shape();
